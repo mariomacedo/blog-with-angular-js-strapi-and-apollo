ba2622aed5513ad2ce6e39c6bd4f7274918e21c9
Mario Macedo <mario.macedo@trsolucoes.com>
2021-01-09 13:14:53 -0300

    strapi blog with angular and apollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { CategoryComponent } from './category/category.component';
+import { GraphQLModule } from './graphql.module';
+import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ArticlesComponent },
+  { path: 'article/:id', component: ArticleComponent },
+  { path: 'category/:id', component: CategoryComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent,
+    NavComponent,
+    ArticleComponent,
+    CategoryComponent,
   ],
   imports: [
+    MarkdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import { Subscription } from 'rxjs';
+import ARTICLE_QUERY from '../apollo/queries/article/article';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss'],
+})
+export class ArticleComponent implements OnInit {
+  data: any = {};
+  loading = true;
+  errors: any;
+
+  private queryArticle: Subscription = new Subscription();
+
+  constructor(private apollo: Apollo, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.queryArticle = this.apollo
+      .watchQuery({
+        query: ARTICLE_QUERY,
+        variables: {
+          id: this.route.snapshot.paramMap.get('id'),
+        },
+      })
+      .valueChanges.subscribe((result) => {
+        this.data = result.data;
+        this.loading = result.loading;
+        this.errors = result.errors;
+      });
+  }
+
+  ngOnDestroy() {
+    this.queryArticle.unsubscribe();
+  }
+}
